e14ac304813016dea7d89d61da1fd3838eaa5fb9
bwithbala <balakrishnan.tharumar@gmail.com>
2014-07-09 16:57:25 -0400

    Commit

--- ballzNews9999/WebContent/locals/resources/newsPage.js
+++ ballzNews9999/WebContent/locals/resources/newsPage.js
@@ -14,7 +14,7 @@ jQuery.sap.require("locals.resources.feedList");
 			}).addEventDelegate({
 				onBeforeShow: function(evt) {
 					newsLink = evt.data.payloadInfo;
-					$('#container').load(newsLink);
+					//$('#container').load(newsLink);
 				//	alert("News Link from News Page:" +newsLink);
 				}
 			});
